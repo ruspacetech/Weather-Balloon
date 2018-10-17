b97b47e202d3349dc526074ce7cbe3e3dc742a76
vivekvidyarthi99 <31702922+vivekvidyarthi99@users.noreply.github.com>
2018-10-16 21:43:47 -0400

    Update DHT11.py

--- DHT11.py
+++ DHT11.py
@@ -1,22 +1,22 @@
 #!/usr/bin/python
 
-import sys
-import Adafruit_DHT
-from time import sleep
+#import sys
+#import Adafruit_DHT
+#from time import sleep
 
-i = 10800
+#i = 10800
 
-file=open("DHT_Data.txt","w")
+#file=open("DHT_Data.txt","w")
 
 
-while i > 0:
-    if i == 10799:
-        print("DHT is operating normally")
-    humidity, temperature = Adafruit_DHT.read_retry(11,4)
-    file.write('Temp :{0:0.1f} C Humidity:{1:0.1f} \n'.format(temperature,humidity))
-    i=i-1
+#while i > 0:
+#    if i == 10799:
+#        print("DHT is operating normally")
+#    humidity, temperature = Adafruit_DHT.read_retry(11,4)
+#    file.write('Temp :{0:0.1f} C Humidity:{1:0.1f} \n'.format(temperature,humidity))
+#    i=i-1
 
-=======
+
 # This program initializes and records data from the density humidity temperature sensor (DHT11)
 
 # Source website for more information is http://www.circuitbasics.com/how-to-set-up-the-dht11-humidity-sensor-on-the-raspberry-pi/
